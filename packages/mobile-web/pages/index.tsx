8a18f70e98208e3697446e726274c6ec672801af
limi.chory <limi.chory@gmail.com>
2023-12-09 02:09:08 +0900

    Fix login context

--- packages/mobile-web/pages/index.tsx
+++ packages/mobile-web/pages/index.tsx
@@ -6,44 +6,15 @@ import styles from '@style/Home.module.css'
 import { useLoginContext } from '@context'
 import { ROUTES } from '@util'
 import { ModalPopup, MySettings } from '@component'
-import { useAxios } from '@hook'
-
-import { UserResponseDto } from '../__generated__/Api'
 
 const Home: React.FC = () => {
-  const { isLoggedIn } = useLoginContext()
+  const { me, isLoggedIn } = useLoginContext()
   const router = useRouter()
   const [isSettingOpen, setIsSettingOpen] = useState<boolean>(false)
-  const { api } = useAxios()
-  const [me, setMe] = useState<UserResponseDto | null>()
-  const [callMeCompleted, setCallMeCompleted] = useState<boolean>(false)
 
   useEffect(() => {
     if (!isLoggedIn) router.push(ROUTES.login)
   }, [isLoggedIn, router])
-
-  const fetchMe = async () => {
-    try {
-      const response = await api.me.getMe()
-
-      if (!response.ok) {
-        setCallMeCompleted(false)
-        return
-      }
-
-      const data = response.data as any
-      if (data?.me) {
-        setMe(data.me)
-        setCallMeCompleted(true)
-      }
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  useEffect(() => {
-    fetchMe()
-  }, [callMeCompleted])
 
   return (
     <div className={styles.container}>
--- packages/mobile-web/src/contexts/LoginContext.tsx
+++ packages/mobile-web/src/contexts/LoginContext.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 
 import { useAxios, useCookies } from '@hook'
+import { UserResponseDto } from '@api-type'
 
 import { CookieKeys } from './types'
+import { API_URL } from '../../config'
 
 type LoginContextType = Partial<{
-  userId: string
+  me: UserResponseDto | null
   loginToken: string
   isLoggedIn: boolean
   setLoginTokenCookie: (token: string, expires: Date) => void
@@ -14,24 +17,47 @@ type LoginContextType = Partial<{
 const LoginContext = createContext<LoginContextType>({})
 
 export const LoginContextProvider = ({ children }: PropsWithChildren) => {
-  const [cookies, setCookie] = useCookies([CookieKeys.LoginToken])
+  const [cookies, setCookie, removeCookie] = useCookies([CookieKeys.LoginToken])
   const cookieLoginToken = cookies[CookieKeys.LoginToken]
   const { setAuthorization } = useAxios()
 
   const browser = typeof window !== 'undefined'
   const [loginToken, setLoginToken] = useState<string>(browser && cookieLoginToken)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!loginToken)
+  const [me, setMe] = useState<UserResponseDto | null>()
+
+  const fetchMe = async () => {
+    try {
+      const response = (await axios.get(`${API_URL}/me`, { headers: { Authorization: `Bearer ${loginToken}` } })) as any
+      if (response.data?.me) {
+        setIsLoggedIn(true)
+        setMe(response.data.me)
+      } else {
+        setIsLoggedIn(false)
+        removeCookie(CookieKeys.LoginToken)
+      }
+    } catch (e) {
+      // console.error(e)
+      setIsLoggedIn(false)
+      removeCookie(CookieKeys.LoginToken)
+    }
+  }
 
   useEffect(() => {
     setAuthorization(loginToken)
   }, [loginToken, setAuthorization])
 
-  const isLoggedIn = Boolean(loginToken)
+  useEffect(() => {
+    setIsLoggedIn(true)
+    fetchMe()
+  }, [loginToken])
+
   const setLoginTokenCookie = (token: string, expires: Date) => {
     setCookie(CookieKeys.LoginToken, token, { expires })
     setLoginToken(token)
   }
 
-  return <LoginContext.Provider value={{ loginToken, isLoggedIn, setLoginTokenCookie }}>{children}</LoginContext.Provider>
+  return <LoginContext.Provider value={{ me, loginToken, isLoggedIn, setLoginTokenCookie }}>{children}</LoginContext.Provider>
 }
 
 export const useLoginContext = () => useContext(LoginContext)
