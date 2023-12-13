4db6589e3cd0d78f4c7679619fb2fa5fdd816acf
limi.chory <limi.chory@gmail.com>
2023-12-13 21:58:19 +0900

    Add cors options

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
 
   const config = new DocumentBuilder().setTitle('Waiting API').setDescription('Rest API for Waiting').setVersion('1.0').addBearerAuth().build()
 
